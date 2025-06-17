c982095fbf9ec4aa01eb5328e11f358e523cc121
Austin Pan <22647854+austin-pan@users.noreply.github.com>
2025-06-17 02:34:04 -0700

    prettify initial loading and error pages

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import Icon from "@mdi/react";
+import { mdiSleep } from "@mdi/js";
 
 import RatingGraph from "../../components/RatingGraph/RatingGraph.jsx";
 import MatchHistory from "../../components/MatchHistory/MatchHistory.jsx";
@@ -30,10 +32,10 @@ const Home = () => {
 
   useEffect(() => {
     const heartbeatKey = setInterval(() => {
-      fetch(`${API_URL}/heartbeat/`, {
+      fetch(`${API_URL}/`, {
         mode: "cors"
       });
-    }, 10 * 60 * 1000); // Every 10 minutes
+    }, 5 * 1000); // Every 10 minutes
 
     return () => {
       clearInterval(heartbeatKey);
@@ -54,27 +56,81 @@ const Home = () => {
     fetchData();
   }, [])
 
-  if (error) return <p>A network error was encountered</p>;
-  if (isLoading) return <p>Loading... </p>;
+  if (error) return <ErrorPage />;
   return (
     <div className={styles.content}>
       <h1>Leaderboard</h1>
-      <div className={styles.stats}>
-        <Leaderboard players={players} />
-        <RatingGraph data={timeseries} players={players} />
-      </div>
+      {
+        isLoading ?
+        <LoadingIcon /> :
+        <div className={styles.stats}>
+          <Leaderboard players={players} />
+          <RatingGraph data={timeseries} players={players} />
+        </div>
+      }
 
       <h1>Match History</h1>
-      <MatchHistory games={games} players={players} />
+      {
+        isLoading ?
+        <LoadingIcon /> :
+        <MatchHistory games={games} players={players} />
+      }
 
       <h1>Forms</h1>
       <h2>Add Game</h2>
-      <GameForm players={players} refreshData={refreshData} />
+      {
+        isLoading ?
+        <LoadingIcon /> :
+        <GameForm players={players} refreshData={refreshData} />
+      }
 
       <h2>Add Player</h2>
-      <PlayerForm players={players} refreshData={refreshData} />
+      {
+        isLoading ?
+        <LoadingIcon /> :
+        <PlayerForm players={players} refreshData={refreshData} />
+      }
     </div>
   )
 }
+
+const LoadingIcon = () => {
+  // from https://tensor-svg-loaders.vercel.app/
+  return (
+    <svg viewBox="0 0 50 50" className={styles.loadingIcon}>
+      <circle cx="10" cy="25" r="2">
+        <animate attributeName="cy" values="25;20;25;30;25" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+        <animate attributeName="opacity" values="1;0.3;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+      </circle>
+      <circle cx="18" cy="25" r="2">
+        <animate attributeName="cy" values="25;20;25;30;25" dur="1s" begin="0.1s" repeatCount="indefinite"></animate>
+        <animate attributeName="opacity" values="1;0.3;1" dur="1s" begin="0.1s" repeatCount="indefinite"></animate>
+      </circle>
+      <circle cx="26" cy="25" r="2">
+        <animate attributeName="cy" values="25;20;25;30;25" dur="1s" begin="0.2s" repeatCount="indefinite"></animate>
+        <animate attributeName="opacity" values="1;0.3;1" dur="1s" begin="0.2s" repeatCount="indefinite"></animate>
+      </circle>
+      <circle cx="34" cy="25" r="2">
+        <animate attributeName="cy" values="25;20;25;30;25" dur="1s" begin="0.30000000000000004s" repeatCount="indefinite"></animate>
+        <animate attributeName="opacity" values="1;0.3;1" dur="1s" begin="0.30000000000000004s" repeatCount="indefinite"></animate>
+      </circle>
+      <circle cx="42" cy="25" r="2">
+        <animate attributeName="cy" values="25;20;25;30;25" dur="1s" begin="0.4s" repeatCount="indefinite"></animate>
+        <animate attributeName="opacity" values="1;0.3;1" dur="1s" begin="0.4s" repeatCount="indefinite"></animate>
+      </circle>
+    </svg>
+  )
+}
+
+
+const ErrorPage = () => {
+  return (
+    <div className={styles.error}>
+      <h1>An error has occurred</h1>
+      <p>The backend probably went to sleep and is now waking up. Hopefully refreshing shortly will fix everything!</p>
+        <Icon path={mdiSleep} className={styles.icon} />
+    </div>
+  );
+};
 
 export default Home;
