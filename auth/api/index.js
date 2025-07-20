aae6c7964410d414556bd9dc3177b10e2e674fc5
Austin Pan <22647854+austin-pan@users.noreply.github.com>
2025-07-20 05:31:24 -0700

    update `vercel.json`

--- auth/api/index.js
+++ auth/api/index.js
@@ -82,7 +82,11 @@ app.get('/user', (req, res) => {
   }
 });
 
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// const PORT = 4000;
+// app.listen(PORT, () => {
+    // console.log(`Server running on port ${PORT}`);
+// });
+
+export default (req, res) => {
+  app(req, res);
+};
