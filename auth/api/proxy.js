3e1b186967da15acbb35218e9583f6ca0a54e71f
Austin Pan <22647854+austin-pan@users.noreply.github.com>
2025-07-20 20:07:01 -0700

    change origin in proxy

--- auth/api/proxy.js
+++ auth/api/proxy.js
@@ -2,6 +2,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const serviceProxy = createProxyMiddleware({
   target: process.env.SERVICE_URL,
+  changeOrigin: true,
   pathRewrite: {
     '^/api': ''
   },
@@ -11,6 +12,8 @@ const serviceProxy = createProxyMiddleware({
       res.status(500).json({ error: 'Failed to connect to the API server' });
     },
     proxyReq: (proxyReq, req) => {
+      console.log(process.env.SERVICE_URL);
+      console.log(proxyReq.path);
       proxyReq.setHeader('X-Auth-Token', process.env.SHARED_SECRET);
     }
   }
